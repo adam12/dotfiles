0e7d71f9241d72df1c0488f68671ed3f2f036ad8
Adam Daniels <adam@mediadrive.ca>
2025-06-23 14:04:21 -0500

    Don't use launcher for ruby-lsp

--- .config/nvim/lua/plugins/ruby_lsp.lua
+++ .config/nvim/lua/plugins/ruby_lsp.lua
@@ -4,7 +4,7 @@ return {
   dir = "~/code/github.com/adam12/ruby-lsp.nvim",
   opts = {
     -- auto_install = true,
-    use_launcher = true,
+    use_launcher = false,
     autodetect_tools = true,
   },
 }
