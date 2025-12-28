48d8a0757ba6a618b44df1834fc6bbded8d0d071
Adam Daniels <adam@mediadrive.ca>
2025-12-28 15:59:23 -0500

    Add vim-rhubarb dep

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,6 +1,8 @@
 return {
   'tpope/vim-fugitive',
+  'tpope/vim-rhubarb', -- GitHub extension for fugitive
   'pocke/rbs.vim',
   'tpope/vim-sleuth',
   'vim-crystal/vim-crystal',
+  'slim-template/vim-slim',
 }
