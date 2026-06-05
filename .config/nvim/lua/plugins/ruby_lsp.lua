d5bbbc064c8bb243d648ef69f6a0311774f47cc4
Adam Daniels <adam@mediadrive.ca>
2026-06-05 10:24:44 -0400

    Remove deprecated launcher flag

--- .config/nvim/lua/plugins/ruby_lsp.lua
+++ .config/nvim/lua/plugins/ruby_lsp.lua
@@ -4,7 +4,6 @@ return {
   dir = "~/code/github.com/adam12/ruby-lsp.nvim",
   opts = {
     -- auto_install = true,
-    use_launcher = false,
     autodetect_tools = true,
     lspconfig = {
       init_options = {
